95aea011f43bacfcf4a40c0eb489fabd186a3820
m11k08 <bear.kohei@gmail.com>
2020-01-14 13:53:58 +0900

    bug fixed

--- brat_clip.sh
+++ brat_clip.sh
@@ -1,6 +1,6 @@
 TMP_FILE='_TMP_crop.pdf'
 FNAME=$1
 OUT=`echo $FNAME | awk -F '.' '{print($1)}'`'-crop.pdf'
-pdfcrop --mergins "0 -30 0 0" FNAME TMP_FILE
-pdfcrop TMP_FILE OUT
-rm TMP_FILE
+pdfcrop --margins "0 -30 0 0" $FNAME $TMP_FILE
+pdfcrop $TMP_FILE $OUT
+rm $TMP_FILE
